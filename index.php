a31bfc4f09039653119296a38e55157ed64eada3
Robin <robin@norrland.xyz>
2023-01-12 23:36:25 +0100

    added https to jquery

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<div class="result"></div>
 </body>
 
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
 <script>
     function refresh_time() {
         jQuery.ajax({
